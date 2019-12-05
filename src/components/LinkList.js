b2feebbe8f9cf1c27725791df6378849f0856195
Igor Aragão <roginaldosemog@gmail.com>
2019-12-05 10:28:14 -0300

    Implement store update after insertion

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,27 +4,27 @@ import Link from "./Link";
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const FEED_QUERY = gql`
-  {
-    feed {
-      links {
-        id
-        createdAt
-        url
-        description
-        postedBy {
-          id
-          name
+export const FEED_QUERY = gql`
+    {
+        feed {
+            links {
+                id
+                createdAt
+                url
+                description
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
         }
-        votes {
-          id
-          user {
-            id
-          }
-        }
-      }
     }
-  }
 `
 
 class LinkList extends Component {
@@ -40,13 +40,27 @@ class LinkList extends Component {
                     return (
                         <div>
                             {linksToRender.map((link, index) => (
-                              <Link key={link.id} link={link} index={index} />
+                                <Link
+                                    key={link.id}
+                                    link={link}
+                                    index={index}
+                                    updateStoreAfterVote={this._updateCacheAfterVote}
+                                />
                             ))}
                         </div>
                     )
                 }}
             </Query>
         )
+    }
+
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        const data = store.readQuery({ query: FEED_QUERY })
+
+        const votedLink = data.feed.links.find(link => link.id === linkId)
+        votedLink.votes = createVote.link.votes
+
+        store.writeQuery({ query: FEED_QUERY, data })
     }
 }
 
