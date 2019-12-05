1fa89828a0aef040f4b183c8b357b80cf7bd97d3
Igor Aragão <roginaldosemog@gmail.com>
2019-12-05 09:17:57 -0300

    Require authentication with server

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
-import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter } from 'react-router-dom';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { createHttpLink } from 'apollo-link-http';
+
+import * as serviceWorker from './serviceWorker';
+import { AUTH_TOKEN } from './constants';
+import App from './components/App';
+import './styles/index.css';
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 })
 
